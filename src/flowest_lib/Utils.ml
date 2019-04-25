8b0f1022bcdeb4dfef58d7f9e1b4b6b6a42f7f77
Bannerets <comonoid@protonmail.com>
2019-04-25 06:02:51 +0300

    Remove unused Utils.ml(i).

--- src/flowest_lib/Utils.ml
+++
@@ -1,5 +0,0 @@
-open! Base
-
-let first (a, b) ~f = (f a, b)
-
-let second (a, b) ~f = (a, f b)
--- src/flowest_lib/Utils.mli
+++
@@ -1,7 +0,0 @@
-open! Base
-
-val first: 'a * 'b -> f:('a -> 'c) -> 'c * 'b
-(** [first] from Haskell *)
-
-val second: 'a * 'b -> f:('b -> 'c) -> 'a * 'c
-(** [second] from Haskell *)
